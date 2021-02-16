ceead518ea0c4450b36902e13093f7f4e8fe1b35
Pawan Bharti <bpawan96@gmail.com>
2021-02-17 02:21:01 +0530

    Added show and delete posts functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
 
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show, :destroy]
   
   get 'home/index'
   
