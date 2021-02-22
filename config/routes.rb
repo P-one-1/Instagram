862a7bc22abd71cc853b10d67f8de8886fcac1b3
Pawan Kumar <bpawan96@gmail.com>
2021-02-23 01:27:12 +0530

    create and search tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
 
   resources :posts, only: [:new, :create, :show, :destroy]
   
   get 'home/index'
+
+  get 'search' => 'search#index'
   
   root to: 'home#index'
 end
